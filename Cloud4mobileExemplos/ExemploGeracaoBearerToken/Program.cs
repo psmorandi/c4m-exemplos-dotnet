7537893c6ba0113d36a4fede859cb89ca160e9c4
Paulo Morandi <paulomorandi@mobiltec.com.br>
2021-08-30 10:45:44 -0300

    Ajustando código e adiconando info de como obter as chaves de acesso.

--- Cloud4mobileExemplos/ExemploGeracaoBearerToken/Program.cs
+++ Cloud4mobileExemplos/ExemploGeracaoBearerToken/Program.cs
@@ -9,12 +9,14 @@
         {
             Console.WriteLine("Exemplo de geração de token de autenticação.");
 
-            var url = "https://api.cloud4mobile.com.br/devices?status=1";
-            var method = "GET";
-            var consumerKey = "<insira_aqui>";
-            var consumerSecret = "<insira_aqui>";
+            const string Url = "https://api.cloud4mobile.com.br/devices?status=1";
+            const string Method = "GET";
+            
+            // consumer key e secret podem ser obitidos aqui: https://admin.cloud4mobile.com.br
+            const string ConsumerKey = "<insira_aqui>";
+            const string ConsumerSecret = "<insira_aqui>";
 
-            var token = TokenGenerator.GetBearerToken(url, method, consumerKey, consumerSecret);
+            var token = TokenGenerator.GetBearerToken(Url, Method, ConsumerKey, ConsumerSecret);
 
             Console.WriteLine("---");
             Console.WriteLine("HTTP header a ser utilizado: ");
