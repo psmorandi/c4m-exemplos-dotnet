e29521fa8fe6dfa63af74385885644e931537194
Paulo Morandi <paulomorandi@mobiltec.com.br>
2021-08-31 17:02:42 -0300

    Simplificando o código para deixar mais didático.

--- Cloud4mobileExemplos/ExemploListarDispositivos/Program.cs
+++ Cloud4mobileExemplos/ExemploListarDispositivos/Program.cs
@@ -11,21 +11,17 @@
         {
             Console.WriteLine("Exemplo para listar os dispositivos ativos com os detalhes do grupo e dados da coleta de bateria.");
 
-            const string baseUrl = "https://api.cloud4mobile.com.br";
-            const string resource = "devices";
-            const string parameters = "status=1&$expand=Group,LastBatteryData";
-            var url = $"{baseUrl}/{resource}?{parameters}";
+            const string url = "https://api.cloud4mobile.com.br/devices?status=1&$expand=Group,LastBatteryData";
             Console.WriteLine($"URL: {url}");
-            const string method = "GET";
-            
+
             // consumer key e secret podem ser obitidos aqui: https://admin.cloud4mobile.com.br
             const string consumerKey = "<insira_aqui>";
             const string consumerSecret = "<insira_aqui>";
 
-            var token = TokenGenerator.GetBearerToken(url, method, consumerKey, consumerSecret);
+            var token = TokenGenerator.GetBearerToken(url, "GET", consumerKey, consumerSecret);
 
-            var client = new RestClient(baseUrl) { Authenticator = new JwtAuthenticator(token) };
-            var request = new RestRequest($"{resource}?{parameters}", DataFormat.Json);
+            var client = new RestClient("https://api.cloud4mobile.com.br") { Authenticator = new JwtAuthenticator(token) }; // Authorization: Bearer <token>
+            var request = new RestRequest("devices?status=1&$expand=Group,LastBatteryData", DataFormat.Json);
             var result = client.ExecuteAsGet<string>(request, "GET");
             Console.WriteLine(result.Content);
             Console.ReadKey();
